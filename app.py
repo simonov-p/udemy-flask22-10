8b021e6d53c6da737ef9debf4e4f11ac5d644349
Петр Симонов (kraud.ru) <simonov@kraud.ru>
2022-10-06 01:04:32 +0300

    65. General improvements to our first REST API

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+from email import message
 import imp
-
-
 import uuid
+
+from flask_smorest import abort
 from flask import Flask, request
 from db import items, stores
 
@@ -16,6 +17,15 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'name' is included in the JSON payload."
+        )
+        
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store {store['name']} already exists")
     store_id = uuid.uuid4().hex
     new_store = {**store_data, "id": store_id}
     stores[store_id] = new_store
@@ -27,14 +37,29 @@ def get_store(store_id):
     try:
         return stores[store_id], 201
     except KeyError:
-        return {"message": "Store not found"}, 404 
+        abort(404, message="Store not found")
 
 
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload."
+        )
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(404, message="Item already exists.")
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
 
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
@@ -53,4 +78,4 @@ def get_item(item_id):
     try:
         return items[item_id], 201
     except KeyError:
-        return {"message": "Item not found"}, 404 
+        abort(404, message="Item not found")
