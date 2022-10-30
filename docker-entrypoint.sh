20e9bbe3969f76925bd2177b1441a643fdd173b1
Петр Симонов (kraud.ru) <simonov@kraud.ru>
2022-10-30 23:06:12 +0300

    try to fix sqlachemy error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 flask db upgrade
 
-exec gunicorn --bind 0.0.0.0:80 "app:create_app()"
+exec gunicorn --bind 0.0.0.0:80 "app:create_app(postgresql://fxjjtjqr:n4xP2sPgmSVrJdOcmHpzFt8u4xKzPB4L@lucky.db.elephantsql.com/fxjjtjqr)"
